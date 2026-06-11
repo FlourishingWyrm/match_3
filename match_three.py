96fc94c1d7ff9d1f650248d05c34bd2bc4219a91
FlourishingWyrm <141595341+FlourishingWyrm@users.noreply.github.com>
2026-06-11 13:39:16 +1200

    Add files via upload

--- match_three.py
+++ match_three.py
@@ -1,31 +1,30 @@
+import os
+import platform
 from tkinter import *
 import time
 import math
-from functools import partial
-import random
-import os
-from PIL import Image
-from fractions import Fraction
+game_state = ["none",1.0,3,0]
+colours = ["#FF0000", "#0000FF", "#9D00FF", "#FF8000", "#32CD32", "#FF0000", "#0000FF", "#9D00FF", "#FF8000", "#32CD32"]
+seed = [6, 1]
 
-from samba import colour
-colours = ["#FF0000","#0000FF", "#FF0000","#0000FF","#FF0000","#0000FF","#FF0000","#0000FF","#FF0000","#0000FF","#FF0000","#0000FF",]
-seed = [6,1]
-def pos_to_cor(x,y):
+
+def pos_to_cor(x, y):
     """converts the pixel position to the location on the grid"""
-    return int((x -740)/100) , int((y - 400)/100)
+    return int((x - 740) / 100), int((y - 400) / 100)
 
-def cor_to_pos(x,y):
+
+def cor_to_pos(x, y):
     """converts the location on the grid to the pixel position on the screen"""
-    return (x * 100) + 740, (y*100)+400
-
+    return (x * 100) + 740, (y * 100) + 400
 
 
 def start_drag(event):
     # Store the initial position of the widget when dragging starts
     event.widget.startX = event.x  # Store initial X position
     event.widget.startY = event.y  # Store initial Y position
-    event.widget.trux =  event.widget.winfo_x()
-    event.widget.truy =  event.widget.winfo_y()
+    event.widget.trux = event.widget.winfo_x()
+    event.widget.truy = event.widget.winfo_y()
+
 
 def on_drag(event):
     # Update widget position as it's dragged
@@ -34,14 +33,16 @@ def on_drag(event):
     x = widget.winfo_x() - widget.startX + event.x
     y = widget.winfo_y() - widget.startY + event.y
     # updates the position
-    widget.place(x=x,y=y)
+    widget.place(x=x, y=y)
     widget.bind("<ButtonRelease-1>", snap)
 
-#https://www.reddit.com/r/algorithms/comments/3byyto/best_way_to_find_the_leftmost_first_digit_of_an/
+
+# https://www.reddit.com/r/algorithms/comments/3byyto/best_way_to_find_the_leftmost_first_digit_of_an/
 def leftMostDigit(n):
     """finds the left most diget of a number"""
     highestPower10 = math.floor(math.log10(n))
-    return n / (10**highestPower10)
+    return n / (10 ** highestPower10)
+
 
 def snap(event):
     """snaps back to the grid and swaps the two tiles (if applicable)"""
@@ -56,49 +57,54 @@ def snap(event):
     # unreachable by normal computers
     most = 9999999999999999999999999999999999999
     # converts the pixel position to a position on the grid this could be unnessary ut it makes the math a tad easier to understand
-    locs = pos_to_cor(sx,sy)
+    locs = pos_to_cor(sx, sy)
     # acceptable places for the tile to be moved to
     dists = []
     # essentaly creates a 3x3 grid centered in the start location of tthe piece selected in the grid format
-    for t in [1,0,-1]:
-        for l in [1,0,-1]:
-            dists.append(cor_to_pos(locs[0]+l, locs[1]+t))
+    for t in [1, 0, -1]:
+        for l in [1, 0, -1]:
+            dists.append(cor_to_pos(locs[0] + l, locs[1] + t))
     for i in dists:
         # calculates the distance using fun maths
-        prop = math.sqrt((x-i[0])*(x-i[0])+(y-i[1])*(y-i[1]))
+        prop = math.sqrt((x - i[0]) * (x - i[0]) + (y - i[1]) * (y - i[1]))
         # if the distance is the shortest its x and y values will be used
-        if prop < most and [i[0],i[1]] in acc:
+        if prop < most and [i[0], i[1]] in acc:
             most = prop
-            fx,fy = i
+            fx, fy = i
     # thinking code (code written to understand ideas)
-    pos_to_cor(fx,fy)
+    pos_to_cor(fx, fy)
 
     # swaps the two tiles to be swapped
-    widget.place(x=fx,y=fy)
-    sec = pos_to_cor(fx+100,fy+100)
-    grid[sec[1]][sec[0]][0].place(x=sx,y=sy)
-    grid[sec[1]][sec[0]],grid[locs[1]+1][locs[0]+1] = grid[locs[1]+1][locs[0]+1],grid[sec[1]][sec[0]]
-    check()
+    widget.place(x=fx, y=fy)
+    sec = pos_to_cor(fx + 100, fy + 100)
+    grid[sec[1]][sec[0]][0].place(x=sx, y=sy)
+    grid[sec[1]][sec[0]], grid[locs[1] + 1][locs[0] + 1] = grid[locs[1] + 1][locs[0] + 1], grid[sec[1]][sec[0]]
+    check(1)
 
-def check():
+
+def check(a=0):
     """checks for matches (ie 3 in a row)"""
-    for x in range(1,6):
-        for y in range(1,6):
+    for x in range(1, 6):
+        for y in range(1, 6):
             try:
-                if grid[x][y][1] == grid[x][y+1][1] == grid[x][y+2][1]: # across check due to the downward check checking out of bounds causing it to be skipped
+                if grid[x][y][1] == grid[x][y + 1][1] == grid[x][y + 2][1]:  # across check due to the downward check checking out of bounds causing it to be skipped
+                    score(grid[x][y][1])
                     grid[x][y + 2][0].destroy()
                     fall_replace([x, y + 2])
-                    time.sleep(0.1) # sleep
-                    root.update() # update
+                    time.sleep(0.1)  # sleep
+                    root.update()  # update
                     grid[x][y + 1][0].destroy()
                     fall_replace([x, y + 1])
-                    time.sleep(0.1) # sleep
-                    root.update() # update
+                    time.sleep(0.1)  # sleep
+                    root.update()  # update
                     grid[x][y][0].destroy()
                     fall_replace([x, y])
-                    time.sleep(0.1) # sleep
-                    root.update() # update
-                    check() # allows for continuous checks
+                    time.sleep(0.1)  # sleep
+                    root.update()  # update
+                    if a == 1:
+                        game_state[0] = grid[x][y][1]
+
+                    check()  # allows for continuous checks
             except IndexError:
                 print("s")
                 continue
@@ -109,19 +115,23 @@ def check():
         for y in range(1, 6):
             try:
                 if grid[x][y][1] == grid[x + 1][y][1] == grid[x + 2][y][1]:
+                    score(grid[x][y][1])
                     grid[x][y][0].destroy()
                     fall_replace([x, y])
-                    time.sleep(0.1) # sleep
-                    root.update() # update
+                    time.sleep(0.1)  # sleep
+                    root.update()  # update
                     grid[x + 1][y][0].destroy()
                     fall_replace([x + 1, y])
-                    time.sleep(0.1) # sleep
-                    root.update() # update
+                    time.sleep(0.1)  # sleep
+                    root.update()  # update
                     grid[x + 2][y][0].destroy()
                     fall_replace([x + 2, y])
-                    time.sleep(0.1) # sleep
-                    root.update() # update
-                    check() # allows for continuous checks
+                    time.sleep(0.1)  # sleep
+                    root.update()  # update
+                    if a == 1:
+                        game_state[0] = grid[x][y][1]
+
+                    check()  # allows for continuous checks
             except IndexError:
                 print("s")
                 continue
@@ -129,24 +139,20 @@ def check():
                 print("xe")
                 continue
 
+
+
 def fall_replace(i):
     try:
         x = i[1]
         y = i[0]
-        if i[1]>0:
-            if i[0] >0:
-
-                print(x,y)
+        if i[1] > 0:
+            if i[0] > 0:
+                print(x, y)
                 print(grid[y][x])
-                grid[y][x] = grid[y-1][x]
+                grid[y][x] = grid[y - 1][x]
                 print(grid[y][x])
                 update()
-                print(fall_replace([y-1,x]))
-
-
-
-
-
+                print(fall_replace([y - 1, x]))
 
                 # print(grid[i[0]-1][i[1]][0])
                 # if grid[i[0]-1][i[1]][0] == ["spawn"]:
@@ -189,7 +195,7 @@ def fall_replace(i):
     except IndexError:
         qx = i[1]
         qy = i[0]
-        print(qx,qy)
+        print(qx, qy)
 
         colour = colours[int(seeder())]
         for f in grid:
@@ -198,7 +204,7 @@ def fall_replace(i):
         star = grid[qy][qx]
         star = str(star)
         print(star)
-        grid[qy][qx] = [Label(root, bg=colour, height=2, width=4, padx=0, pady=0, text="n"),colour,"n"]
+        grid[qy][qx] = [Label(root, bg=colour, height=2, width=4, padx=0, pady=0, text="n"), colour, "n"]
         grid[qy][qx][0].bind("<ButtonPress-1>", start_drag)  # Detect mouse press to start dragging
         grid[qy][qx][0].bind("<B1-Motion>", on_drag)  # Move label while dragging
         print(grid[qy][qx])
@@ -213,51 +219,82 @@ def update():
     try:
         for x in range(1, 6):
             for y in range(1, 6):
-                xe,ye = cor_to_pos(y-1,x-1)
-                grid[x][y][0].place(x = xe,y = ye)
+                xe, ye = cor_to_pos(y - 1, x - 1)
+                grid[x][y][0].place(x=xe, y=ye)
     except IndexError:
         return "a"
     except TypeError:
         print("72.5")
 
 
+def score(type):
+    game_state[2] = 3
+    if game_state[0] == type:
+        game_state[1]+=0.1
+
+    game_state[3]+= 100*game_state[1]
+    game_state[4].config(text=str(game_state[3]),font=("Arial", "25", "bold"))
+    print(str(round(game_state[1],2)),"aljejsbflauevrfi")
+    game_state[5].config(text=str(round(game_state[1],2)))
+    game_state[4].place(x=1610, y=25)
+    root.update()
+    time.sleep(0.1)
+    game_state[4].config(text=str(game_state[3]), font=("Arial", "15"))
+    game_state[4].place(x=1620,y=35)
 
 
-
-    # for item in grid:
-    #     g = []
-    #     for part in item:
-    #         g.append(part)
-    #     print(g)
 def seeder():
     """updates the seed"""
     global seed
-    seed = [seed[1],seed[0]+seed[1]]
+    seed = [seed[1], seed[0] + seed[1]]
     return leftMostDigit(seed[1])
+
+
 def gridset():
     """creates the grid"""
-    global acc,grid
-    grid = [["spawn"]] # to make impossable to reach
+    global acc,grid,game_state
+    grid = [["spawn"]]  # to make impossable to reach
     acc = []
 
     for up in range(5):
-        tmp=[["NONE","",99999]] # to make impossable to reach
-        for a in range(5): # creates a row
-            x,y = ((a*100)+740,(up*100)+400) # creates the co-ordinates
-            colour = colours[int(seeder())] # gets the colour value for the lable (tile)
-            label = Label(root, bg=colour,height=2, width=4, padx=0, pady=0,text=str(a+5*up)) # creates the label
+        tmp = [["NONE", "", 99999]]  # to make impossable to reach
+        for a in range(5):  # creates a row
+            x, y = ((a * 100) + 740, (up * 100) + 400)  # creates the co-ordinates
+            colour = colours[int(seeder())]  # gets the colour value for the lable (tile)
+            label = Label(root, bg=colour, height=2, width=4, padx=0, pady=0, text=str(a + 5 * up))  # creates the label
             label.bind("<ButtonPress-1>", start_drag)  # Detect mouse press to start dragging
             label.bind("<B1-Motion>", on_drag)  # Move label while dragging
-            acc.append([x,y])# appends co-ordinates to an important spot
-            label.place(x=x,y=y) # puts the tile on the grid where it goes
-            tmp.append([label,colour,str(a+5*up)]) # appends the label and its friends to the row
+            acc.append([x, y])  # appends co-ordinates to an important spot
+            label.place(x=x, y=y)  # puts the tile on the grid where it goes
+            tmp.append([label, colour, str(a + 5 * up)])  # appends the label and its friends to the row
 
-        grid.append(tmp) # puts the row on the rows spot, grid
-
+        grid.append(tmp)  # puts the row on the rows spot, grid
+    scoreback = Label(root, bg="#48F07F", width=20, height=3)
+    score = Label(bg="#48F07F",text="0")
+    multi = Label(bg="#00FF00",text="1.0")
+    game_state.append(score)
+    game_state.append(multi)
+    multi.place(x=1600,y=90)
+    score.place(x=1615,y=30)
+    scoreback.place(x=1600, y=15)
 if __name__ == '__main__':
     """who knows what this does"""
     root = Tk()
-    root.title("e")
-    root.attributes("-fullscreen", True)
-    gridset()
+    trial = input("do you want the instructions and licences?")+" "
+    while (not trial[0].lower() == "n" and not trial[0].lower() == "y"):
+        trial = input("please enter either yes or no")+" "
+        print(trial[0].lower()=="n")
+    if trial[0].lower() == "n":
+        print("e")
+        root.title("e")
+        root.attributes("-fullscreen", True)
+        gridset()
+    else:
+        if platform.system() == 'Windows':
+            os.startfile("hi.txt")
+        elif platform.system() == 'Darwin':  # macOS
+            os.system(f'open "hi.txt"')
+        else:  # Linux and other Unix-like systems
+            os.system(f'xdg-open "hi.txt"')
+
     root.mainloop()
